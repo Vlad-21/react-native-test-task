5f003a2b29313ef9fe52743c2d99e7c5605a0bf4
vladkryshchuk <vladkryshchuk20@gmail.com>
2023-06-05 16:44:59 +0300

    fix: data in getFilmList function

--- src/service/HttpService.ts
+++ src/service/HttpService.ts
@@ -1,11 +1,10 @@
-import { IFilm } from '../types/film.types';
+import {IFilm} from '../types/film.types';
 
 const getFilmsList = async (filmName: string): Promise<Array<IFilm>> => {
     const result = await fetch(
         `https://api.tvmaze.com/search/shows?q=${filmName}`
     );
-    const data = await result.json();
-    return data;
+    return await result.json();
 };
 
 export { getFilmsList };
